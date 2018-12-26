4fe5a42f6afce470102fea09fa2b2d9953357725
rob-crane <28930454+Rob-Crane@users.noreply.github.com>
2018-12-26 02:38:22 -0500

    Implement prediction function

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -1,5 +1,7 @@
 #include "kalman_filter.h"
 
+#include <cmath>
+
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
 
@@ -23,15 +25,25 @@ void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, // MatrixXd &F_in,
 }
 
 void KalmanFilter::Predict(long long delta_T) {
-  /**
-   * TODO: Update the state transition matrix F according to the new elapsed time.
-   * Time is measured in seconds.
-   * TODO: Update the process noise covariance matrix.
-   * Use noise_ax = 9 and noise_ay = 9 for your Q matrix.
-   */
-  /**
-   * TODO: predict the state
-   */
+
+  MatrixXd F(4,4);
+  F << 1, 0, delta_T, 0,
+       0, 1, 0,       delta_T,
+       0, 0, 1,       0,
+       0, 0, 0,       1;
+  x_ = F * x_;
+
+  MatrixXd Q(4,4);
+  float noise_ax = 9.0;
+  float noise_ay = 9.0;
+  float d4_coef = pow(delta_T, 4) / 4.0;
+  float d3_coef = pow(delta_T, 3) / 2.0;
+  float d2_coef = pow(delta_T, 2);
+  Q << d4_coef*noise_ax, 0,                d3_coef*noise_ax, 0,
+       0,                d4_coef*noise_ay, 0,                d3_coef*noise_ay,
+       d3_coef*noise_ax, 0,                d2_coef*noise_ax, 0,
+       0,                d3_coef*noise_ay, 0,                d2_coef*noise_ay;
+  P_ = F * P_ * F.transpose() + Q;
 }
 
 void KalmanFilter::Update(const VectorXd &z, const MatrixXd &R) {
